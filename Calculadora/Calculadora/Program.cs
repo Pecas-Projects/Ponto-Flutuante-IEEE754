e03b5798720929247102b7c9a3d3d40b2d48e3b5
Ana Tartari <anatartari412@gmail.com>
2020-06-10 12:47:13 -0300

    Projeto

---
+++ Calculadora/Calculadora/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a);
+            sbyte v = Convert.ToSByte(a);
+            byte b = v;
+
+
+            if (a< 0)
+            {
+                Console.WriteLine("este numero e menor que 0");
+            }
+            else
+            {
+                Console.WriteLine("nao e menor");
+            }
+
+
+        }
+    }
+}
