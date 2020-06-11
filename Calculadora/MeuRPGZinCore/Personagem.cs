398d6e15df59beefc1ae10579b6ab42009f3e099
Ana Tartari <anatartari412@gmail.com>
2020-06-11 15:54:08 -0300

    RPG LAPA

---
+++ Calculadora/MeuRPGZinCore/Personagem.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuRPGZinCore
+{
+
+    public class PersonagemEventArgs : EventArgs
+    {
+        public int NivelAnterior { get; set; }
+    }
+
+    public class Personagem
+    {
+        public delegate void PersonagemUpadoEventHandler(object sender, PersonagemEventArgs e);
+
+        public event PersonagemUpadoEventHandler PersonagemUP;
+
+        public int Nivel { get; set; }
+        public int Vida { get; set; }
+
+        private int pontoUpagem = 100;
+
+        public void UparNivel()
+        {
+            int nivelAnt = this.Nivel;
+            Nivel+=2;
+            pontoUpagem += 100;
+            if(this.PersonagemUP != null)
+                this.PersonagemUP(this, new PersonagemEventArgs { NivelAnterior = nivelAnt });
+        }
+
+        public void TirarVida()
+        {
+            Vida--;
+        }
+
+        public void GanharVida()
+        {
+            Vida += 10;
+            if(Vida > pontoUpagem)
+            {
+                UparNivel();
+            }
+        }
+    }
+}
---
+++ Calculadora/MeuRPGZinUWP/MainPage.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using MeuRPGZinCore;
+
+// O modelo de item de Página em Branco está documentado em https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x416
+
+namespace MeuRPGZinUWP
+{
+    /// <summary>
+    /// Uma página vazia que pode ser usada isoladamente ou navegada dentro de um Quadro.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        Personagem p = null;
+        
+        public MainPage()
+        {
+            this.InitializeComponent();
+            BtnCriarDoar.Click += BtnCriarDoar_Click;            
+        }
+
+        private void BtnCriarDoar_Click(object sender, RoutedEventArgs e)
+        {
+            if (p == null)
+            {
+                p = new Personagem { Nivel = 1, Vida = 10 };
+                var btn = sender as Button;
+                btn.Content = "Doar Vida";
+                p.PersonagemUP += TratarPersonagemUPLevel;
+                TblLevel.Text = "Nível:" + p.Nivel;
+                TblLife.Text = "Vida:" + p.Vida;
+            }
+            else
+            {
+                p.GanharVida();
+                TblLife.Text = "Vida:" + p.Vida;
+            }
+        }
+
+        private void TratarPersonagemUPLevel(object sender, PersonagemEventArgs e)
+        {
+            
+            var persona = sender as MeuRPGZinCore.Personagem;
+            TblLevel.Text = "Nível Anterior: " + e.NivelAnterior + " Novo Nível: " + persona.Nivel; 
+        }
+
+        private void BtnRetirar_Click(object sender, RoutedEventArgs e)
+        {
+            p?.TirarVida();
+            TblLife.Text = "Vida:" + p?.Vida;
+        }
+
+        private void BtnVai_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Pagina2), p);
+        }
+
+        //private void MeuBotao_Click(object sender, RoutedEventArgs e)
+        //{
+        //    var btn = sender as Button;
+        //    if (btn != null)
+        //    {
+        //        double topBotao = Canvas.GetTop(btn);
+
+        //        if (btn.Content.ToString() == "TÓXICO")
+        //        {
+        //            Debug.WriteLine("EU SOU TÓXICO!");
+        //        }
+        //    }
+        //}
+
+        //private void MeuBotao_PointerEntered(object sender, PointerRoutedEventArgs e)
+        //{
+        //    MeuBotao.Foreground = new SolidColorBrush(Windows.UI.Colors.Red);
+
+        //}
+
+        //private void MeuBotao_PointerExited(object sender, PointerRoutedEventArgs e)
+        //{
+        //    // MeuBotao.Foreground = new SolidColorBrush(Windows.UI.Colors.Black);
+        //}
+    }
+}
