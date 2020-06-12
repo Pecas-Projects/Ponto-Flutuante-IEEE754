2176bef62112a2c195da84bcc88b99099581080a
biacalazans <biacalazans@users.noreply.github.com>
2020-06-12 14:30:28 -0300

    fodasse

--- Calculadora/Calculadora/Program.cs
+++ Calculadora/Calculadora/Program.cs
@@ -7,7 +7,7 @@ namespace Calculadora
         static void Main(string[] args)
         {
             int[] bases = { 2, 16 };
-            string input;
+            string input, saida;
             bool negativo = false;
             Console.WriteLine("Digite o número desejado: ");
             input = Console.ReadLine();
@@ -15,50 +15,93 @@ namespace Calculadora
             {
                 negativo = true;
             }
-            string saida = Dec_toBin_32bits(input, negativo);
+           /* string saida = Dec_toBin_32bits(input, negativo);
             Console.WriteLine("O valor em notação ponto flutuante de 32 bits é " + saida);
-            saida = Dec_toBin_8bits(input, negativo);
+            saida = Dec_toBin_8bits(input);
             Console.WriteLine("O valor em notação ponto flutuante de 8 bits é " + saida);
             saida = Dec_toBin_64bits(input, negativo);
-            Console.WriteLine("O valor em notação ponto flutuante de 64 bits é " + saida);
+            Console.WriteLine("O valor em notação ponto flutuante de 64 bits é " + saida); */
+            
+            saida = Dec_toBin_8bits(input);
+            Console.WriteLine("O valor em notação ponto flutuante de 8 bits é " + saida);
+
+
         }
 
-        static public string Dec_toBin_8bits(string value, bool negativo) //falta implementar o excesso
+        static public string execesso(int expoente)
         {
-            string expoente = null, resultado = null, mantissa = null;
+            string resultado = null;
+
+            if (expoente == -4) resultado = "000";
+            else if (expoente == -3) resultado = "001";
+            else if (expoente == -2) resultado = "010";
+            else if (expoente == -1) resultado = "011";
+            else if (expoente == 0) resultado = "100";
+            else if (expoente == 1) resultado = "101";
+            else if (expoente == 2) resultado = "110";
+            else if (expoente == 3) resultado = "111";
+            else if (expoente > 3) resultado = "Overflow";
+            else if (expoente < -1) resultado = "Underflow";
+
+            return resultado;
+        }
+
+        static public string Dec_toBin_8bits(string value) //falta implementar o excesso
+        {
+            string expoente = null, resultado = null, mantissa = null, aux = null, fracao;
             double valor = Double.Parse(value);
             Console.WriteLine(valor);
-            double fracionaria, proxNum = valor; //proximo numero a ser dividido
-            int exp = 0, multiplicacoes = 0;
-            if (valor > 2)
+            double fracionaria;
+            bool arredondado = false;
+            int exp = 0;
+
+            //pega a parte inteira e converte para binário obtendo uma parte da mantissa
+            
+            aux = Math.Floor(valor).ToString();
+
+            fracionaria = valor - Math.Floor(valor);
+
+            if(aux != "0")
             {
-                while (true) //divisões sucessivas
-                {
-                    proxNum = proxNum / 2;
-                    exp++;
-                    if (proxNum < 2)
-                    {
-                        break; //controla quando as divisões pararão
-                    }
-                }
+                mantissa = Convert.ToString(Convert.ToByte(aux), 2);
             }
-            else if (valor < 1) //multiplicações sucessivas
+            
+            
+            if(mantissa != null) //se o valor for > 1, o expoente é definido pelo tamanho em binário da parte inteira do valor
             {
-                while (true)
-                {
-                    proxNum = proxNum * 2;
-                    exp--;
-                    if (proxNum >= 1)
-                    {
-                        break;
-                    }
-                }
+                expoente = execesso(mantissa.Length);
+                if (expoente == "Overflow" || expoente == "Underflow") return expoente;
+                
             }
 
-            //obtendo a parte após a vírgula em binário || 2ª etapa
-            fracionaria = proxNum - 1; //obtenção da parte fracionária do quociente
+            //descorir quantos zeros tem depois da virgula
+            else if (mantissa == null)
+            {
+                fracao = fracionaria.ToString();
+                for (int i = 2; i < fracao.Length ; ++i) //começa na primeira casa decimal depois da virgula
+                {
+                    if (fracao[i] == 0) --exp;
+                    else break;
+                }
+
+                expoente = execesso(exp);
+                if (expoente == "Overflow" || expoente == "Underflow") return expoente;
+            }
+
+
+            //pega a parte da mantissa que corresponde ao fracionário
             while (true)
             {
+                if (fracionaria == 0)
+                {
+                    break;
+                }
+
+                if (mantissa != null && mantissa.Length == 4)
+                {
+                    arredondado = true;
+                    break;
+                }
 
                 if (fracionaria >= 1)
                 {
@@ -66,41 +109,19 @@ namespace Calculadora
                 }
                 fracionaria *= 2;
                 mantissa += Math.Floor(fracionaria).ToString();
-                multiplicacoes++;
-                if (multiplicacoes > 4 || fracionaria == 0)
+               
+               
+            }
+
+            if(mantissa.Length < 4)
+            {
+                while(mantissa.Length < 4) //se não tiver 4 digitos na mantissa adiciona zeros no final
                 {
-                    break;
+                    mantissa += "0";
                 }
             }
 
-            //completando 4 bits na mantissa
-            while (mantissa.Length < 4)
-            {
-                mantissa += '0';
-            }
-
-            if (negativo)
-            {
-                resultado = "1";
-            }
-            else
-            {
-                resultado = "0";
-            }
-
-            Console.WriteLine(exp);
-            exp += 127; //valor acrescido para o padrão IEEE
-            Console.WriteLine(exp);
-            expoente = Convert.ToString(Convert.ToByte(exp), 2); //expoente em binário
-            if (expoente.Length < 3)
-            {
-                expoente = '0' + expoente;
-            }
-
-            //Console.WriteLine(expoente);
-            //Console.WriteLine(mantissa);
-            resultado = resultado + expoente + mantissa;
-
+            resultado = "0"+ expoente + mantissa;
             return resultado;
         }
 
@@ -270,7 +291,5 @@ namespace Calculadora
             return resultado;
         }
 
-        //depois, fazer uma função de binário para ponto flutuante
-        //função de ponto flutuante (binário) para hexadecimal
     }
 }
